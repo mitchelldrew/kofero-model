ae0b4c02313e5cfc6c4b92425488acaa5c23778f
mitchdrew <mitchdrew@goatse.cx>
2021-02-17 14:06:04 -0800

    master - mdrew: added Move

--- src/commonMain/kotlin/ro/kofe/model/Character.kt
+++ src/commonMain/kotlin/ro/kofe/model/Character.kt
@@ -1,3 +1,3 @@
 package ro.kofe.model
 
-data class Character(val name:String, val health:Int)
+data class Character(val id:Int, val name:String, val health:Int, val moves:List<Move>)
---
+++ src/commonMain/kotlin/ro/kofe/model/Game.kt
@@ -0,0 +1,3 @@
+package ro.kofe.model
+
+data class Game(val id:Int, val name:String, val characters:List<Character>)
