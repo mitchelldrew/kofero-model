5e28dae31b3d915155e109d6c812f140c062ba10
mitchdrew <mitchdrew@goatse.cx>
2021-02-17 14:55:36 -0800

    master - mdrew: added iconUrl

--- src/commonMain/kotlin/ro/kofe/model/Game.kt
+++ src/commonMain/kotlin/ro/kofe/model/Game.kt
@@ -1,3 +1,3 @@
 package ro.kofe.model
 
-data class Game(val id:Int, val name:String, val characterIds:List<Int>)
+data class Game(val id:Int, val name:String, val characterIds:List<Int>, val iconUrl:String)
