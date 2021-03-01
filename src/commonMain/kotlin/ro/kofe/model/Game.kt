30318ef4788ce8ec05a3f2d6ef5c3dd1ea3d1350
mitchdrew <mitchdrew@goatse.cx>
2021-02-28 16:29:21 -0800

    master - mdrew: changed game to Array

--- src/commonMain/kotlin/ro/kofe/model/Game.kt
+++ src/commonMain/kotlin/ro/kofe/model/Game.kt
@@ -1,3 +1,26 @@
 package ro.kofe.model
 
-data class Game(val id:Int, val name:String, val characterIds:List<Int>, val iconUrl:String)
+data class Game(val id:Int, val name:String, val characterIds:Array<Int>, val iconUrl:String) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as Game
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (!characterIds.contentEquals(other.characterIds)) return false
+        if (iconUrl != other.iconUrl) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + name.hashCode()
+        result = 31 * result + characterIds.contentHashCode()
+        result = 31 * result + iconUrl.hashCode()
+        return result
+    }
+}
