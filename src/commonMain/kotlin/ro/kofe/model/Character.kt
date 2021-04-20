91c9039cf90e426f8ce715d25bdef3e8bf52e622
mitchdrew <mitchdrew@goatse.cx>
2021-04-20 13:46:05 -0700

    master - mdrew: attributes in character

--- src/commonMain/kotlin/ro/kofe/model/Character.kt
+++ src/commonMain/kotlin/ro/kofe/model/Character.kt
@@ -1,13 +1,13 @@
 package ro.kofe.model
 
-class Character(id:Int, val name:String, val health:Int, val moveIds:List<Int>, val iconUrl:String): Obj(id) {
+class Character(id:Int, val name:String, val attributes:Map<String,String>, val moveIds:List<Int>, val iconUrl:String): Obj(id) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is Character) return false
         if (!super.equals(other)) return false
 
         if (name != other.name) return false
-        if (health != other.health) return false
+        if (attributes != other.attributes) return false
         if (moveIds != other.moveIds) return false
         if (iconUrl != other.iconUrl) return false
 
@@ -17,13 +17,14 @@ class Character(id:Int, val name:String, val health:Int, val moveIds:List<Int>, 
     override fun hashCode(): Int {
         var result = super.hashCode()
         result = 31 * result + name.hashCode()
-        result = 31 * result + health
+        result = 31 * result + attributes.hashCode()
         result = 31 * result + moveIds.hashCode()
         result = 31 * result + iconUrl.hashCode()
         return result
     }
 
     override fun toString(): String {
-        return "Character(id= '$id', name='$name', health=$health, moveIds=$moveIds, iconUrl='$iconUrl')"
+        return "Character(name='$name', attributes=$attributes, moveIds=$moveIds, iconUrl='$iconUrl')"
     }
+
 }
